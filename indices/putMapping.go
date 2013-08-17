08f8ad7680448f6d221419ff80bc5044929435fe
Beyang Liu <beyang.liu@gmail.com>
2013-08-17 00:16:54 -0700

    put mapping API

--- indices/putMapping.go
+++ indices/putMapping.go
@@ -10,3 +10,67 @@
 // limitations under the License.
 
 package indices
+
+import (
+	"encoding/json"
+	"fmt"
+	api "github.com/sourcegraph/elastigo/api"
+	"reflect"
+	"strings"
+)
+
+type MappingOptions struct {
+	Timestamp  TimestampOptions             `json:"_timestamp"`
+	Properties map[string]map[string]string `json:"properties"`
+}
+
+type TimestampOptions struct {
+	Enabled bool `json:"enabled"`
+}
+
+func PutMapping(index string, typeName string, instance interface{}, opt MappingOptions) error {
+	if opt.Properties == nil {
+		opt.Properties = make(map[string]map[string]string)
+	}
+
+	instanceType := reflect.TypeOf(instance)
+	if instanceType.Kind() != reflect.Struct {
+		return fmt.Errorf("instance kind was not struct")
+	}
+
+	n := instanceType.NumField()
+	for i := 0; i < n; i++ {
+		field := instanceType.Field(i)
+
+		name := field.Tag.Get("json")
+		if name == "-" {
+			continue
+		} else if name == "" {
+			name = field.Name
+		}
+
+		attrMap := make(map[string]string)
+		tag := field.Tag.Get("elastic")
+		if tag == "" {
+			continue
+		}
+		attrs := strings.Split(tag, ",")
+		for _, attr := range attrs {
+			keyvalue := strings.Split(attr, ":")
+			attrMap[keyvalue[0]] = keyvalue[1]
+		}
+		opt.Properties[name] = attrMap
+	}
+
+	body, err := json.Marshal(map[string]MappingOptions{typeName: opt})
+	if err != nil {
+		return err
+	}
+
+	_, err = api.DoCommand("PUT", fmt.Sprintf("/%s/%s/_mapping", index, typeName), string(body))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ indices/putMapping_test.go
@@ -0,0 +1,75 @@
+package indices
+
+import (
+	"encoding/json"
+	api "github.com/sourcegraph/elastigo/api"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+var (
+	mux    *http.ServeMux
+	server *httptest.Server
+)
+
+func setup(t *testing.T) {
+	mux = http.NewServeMux()
+	server = httptest.NewServer(mux)
+
+	serverURL, err := url.Parse(server.URL)
+	if err != nil {
+		t.Fatalf("Error: %v", err)
+	}
+
+	api.Domain = strings.Split(serverURL.Host, ":")[0]
+	api.Port = strings.Split(serverURL.Host, ":")[1]
+}
+
+func teardown() {
+	server.Close()
+}
+
+type TestStruct struct {
+	Id         string `json:"id" elastic:"index:not_analyzed"`
+	DontIndex  string `json:"dontIndex" elastic:"index:no"`
+	Number     int    `json:"number" elastic:"type:integer,index:analyzed"`
+	Omitted    string `json:"-"`
+	NoJson     string `elastic:"type:string"`
+	unexported string
+}
+
+func TestPutMapping(t *testing.T) {
+	setup(t)
+	defer teardown()
+
+	options := MappingOptions{Timestamp: TimestampOptions{Enabled: true}}
+	expValue := map[string]MappingOptions{
+		"myType": MappingOptions{
+			Timestamp: TimestampOptions{Enabled: true},
+			Properties: map[string]map[string]string{
+				"id":        {"index": "not_analyzed"},
+				"dontIndex": {"index": "no"},
+				"number":    {"type": "integer", "index": "analyzed"},
+				"NoJson":    {"type": "string"},
+			},
+		},
+	}
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		var value map[string]MappingOptions
+		json.NewDecoder(r.Body).Decode(&value)
+
+		if !reflect.DeepEqual(expValue, value) {
+			t.Errorf("Expected: %+v, but got: %+v", expValue, value)
+		}
+	})
+
+	err := PutMapping("myIndex", "myType", TestStruct{}, options)
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+}
